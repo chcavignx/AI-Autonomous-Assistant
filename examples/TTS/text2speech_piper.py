841e9c24b48445ad3262aa94539956b23f3d6c3d
chcavignx <ccubi73@gmail.com>
2025-12-27 17:57:22 +0100

    refactor: Refactor text2speech_piper.py and update variable names
    Added shebang and docstring for clarity, reordered imports, and updated variable names from snake_case to uppercase (TEXT_FR, TEXT_EN) for text constants. Cleaned up commented code and improved code readability.

--- examples/TTS/text2speech_piper.py
+++ examples/TTS/text2speech_piper.py
@@ -1,7 +1,13 @@
-from piper import PiperVoice, SynthesisConfig
-import wave
+#!/usr/bin/env python3
+"""
+Example of text to speech using Piper
+"""
+
 import os
+import wave
+
 import sounddevice as sd
+from piper import PiperVoice, SynthesisConfig
 
 # Paths to the model and config files for French and English voices
 DATA_DIR = "../../data/"
@@ -72,24 +78,23 @@ def synthesize_voice(model_path, text):
 
 # Example for the French voice
 model_fr = os.path.join(DATA_DIR, "fr_FR-gilles-low.onnx")
-# text_fr = "Ceci est un test avec voix française utilisant le moteur Piper."
-text_fr = " Je m'appelle Gilles et je suis ravi de vous rencontrer."
-text_fr += " Je suis très heureux de pouvoir parler avec vous aujourd'hui."
-text_fr += " J'espère que vous apprécierez cette démonstration."
-# text_fr = "Et donc, mes chers compatriotes, ne demandez pas ce que votre pays peut faire pour vous, demandez ce que vous pouvez faire pour votre pays."
+# TEXT_FR = "Ceci est un test avec voix française utilisant le moteur Piper."
+TEXT_FR = " Je m'appelle Gilles et je suis ravi de vous rencontrer."
+TEXT_FR += " Je suis très heureux de pouvoir parler avec vous aujourd'hui."
+TEXT_FR += " J'espère que vous apprécierez cette démonstration."
 file_fr = setup_output_filename(
     DATA_DIR, TEST_FILE_NAME, model_fr.split("/")[-1].replace(".onnx", "")
 )
-synthesize_voice_and_save(model_fr, text_fr, file_fr)
-synthesize_voice(model_fr, text_fr)
+synthesize_voice_and_save(model_fr, TEXT_FR, file_fr)
+synthesize_voice(model_fr, TEXT_FR)
 # Example for the English (GB) voice
 model_en = os.path.join(DATA_DIR, "jarvis-medium.onnx")
-# text_en = "This is a test in British English using the Piper engine."
-text_en = " My name is Jarvis and I am delighted to meet you."
-text_en += " I am very happy to be able to speak with you today."
-text_en += " I hope you will enjoy this demonstration."
+# TEXT_EN = "This is a test in British English using the Piper engine."
+TEXT_EN = " My name is Jarvis and I am delighted to meet you."
+TEXT_EN += " I am very happy to be able to speak with you today."
+TEXT_EN += " I hope you will enjoy this demonstration."
 file_en = setup_output_filename(
     DATA_DIR, TEST_FILE_NAME, model_en.split("/")[-1].replace(".onnx", "")
 )
-synthesize_voice_and_save(model_en, text_en, file_en)
-synthesize_voice(model_en, text_en)
+synthesize_voice_and_save(model_en, TEXT_EN, file_en)
+synthesize_voice(model_en, TEXT_EN)
